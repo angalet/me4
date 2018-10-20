3dc6dbbd7c53f68079f8cf1cb6a7cba78c9a42ee
angalet <41893557+angalet@users.noreply.github.com>
2018-10-20 21:21:02 +0300

    Add files via upload

--- money.php
+++ money.php
@@ -1,45 +1,37 @@
 <?php
 $today = date('Y-m-d');
-if(!$argv[1]) {
+if(!isset($argv[1])) {
     echo "Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки} \n";
-    }
+}
 else {
-
-//echo (float)$argv[1];
-if ((float)$argv[1] > 0)
-{
-
-$name = array_slice($argv, 2);
-$price = $argv[1];
-// присваивает <body text='black'>
-//$bodytag = str_replace("%body%", "black", "<body text='%body%'>")
-$price = str_replace(",",".",$price);
-echo $price;
-//var_dump($name);
-$name = implode(" ",$name);
-file_put_contents('money.csv', "$today;$price;$name; \n",FILE_APPEND);
-}
-else if ($argv[1]== '--today') 
-{
-    $summ_today = 0;
-    //echo "Вы хотите посчитать сумму за сегодня? \n";
-    $from_file = file('money.csv');
-    //var_dump($from_file);
-    foreach ($from_file as $from_file){
-        $from_file = explode(";",$from_file);
-        //var_dump($from_file);
-        if ($from_file[0] == $today){
-            $summ_today += $from_file[1];
-            //echo "$summ_today \n";
-        }
-        
+    if ((float)$argv[1] > 0){
+        $name = array_slice($argv, 2);
+        $price = $argv[1];
+        $price = str_replace(",",".",$price);
+        $name = implode(" ",$name);
+        $fp = fopen('money.csv', 'a+');
+        $new_line = array($today,$price,$name);
+        fputcsv($fp, $new_line,';');
+        fclose($fp);
     }
-echo "Сумма за сегодня: $summ_today Р \n";
-}
-else 
-{
+    else if ($argv[1]== '--today') 
+    {
+        $summ_today = 0;
+        if (($handle = fopen('money.csv', "r")) !== FALSE) {
+        while (($from_file = fgetcsv($handle, 1000, ";")) !== FALSE) {
+            if ($from_file[0] == $today){
+                $summ_today += $from_file[1];
+            }
+        
+        }
+        fclose($handle);
+    }
+    echo "Сумма за сегодня: $summ_today Р \n";
+    }
+    else 
+    {
     echo "Введите корректную сумму \n";
-}
+    }
 
 }
 
--- visa.php
+++ visa.php
@@ -1,20 +1,27 @@
 <?php
 $data = file('https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8');
-if(!$argv[1]) {
-    echo "Ошибка! Аргументы не заданы. Введите название страны!";
-    }
-else {
-foreach($data as $value){
-        //echo $value."<br>";
-        $str = explode(",",$value);
-        $str1 = trim($str[1]);
-        $str4 = trim($str[4]);
-        $lev = levenshtein($argv[1], trim($str1,' \"'));
-        if ($lev<=3){
-        echo "Вы имели ввиду ".trim($str1,' \"')."? \n";
-        echo trim($str1,' \"').": ".trim($str4,' \"')."\n";
-
-    }
+if(!isset($argv[1])) {
+    echo "Ошибка! Аргументы не заданы. Введите название страны! \n";
 }
+else {
+    $match_something = false;
+    foreach($data as $value){
+        $str = explode(",",$value);
+        $country_name = trim($str[1],' \"');
+        $visa_type = trim($str[4]);
+        $lev = levenshtein(mb_strtolower($argv[1]), mb_strtolower($country_name),1,1,1);
+        //как это работает? пробовал чтобы например вводя "поль" он находил "польша"
+        //но не находит, выводит вес 4. Но ведь вес вставки я поставил 1, 2*1=2, откуда 4?
+        //пробовал много разных вариантов, методом исключения, логику работы функции не обнаружил.
+        //echo mb_strtolower($country_name)." ".$lev."\n";
+            if ($lev<=2){
+                $match_something = true;
+                echo "Вы имели ввиду ".$country_name."? \n";
+                echo $country_name.": ".trim($visa_type,' \"')."\n";
+            }          
+    }
+    if ($match_something == false){
+        echo "По Вашему запросу ничего не найдено, попробуйте ввести другое название \n";
+    }
 }
 ?>
